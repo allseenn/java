8903d1e7d75e0e2ff06f3b074c761c0dfaa04533
allseen <allseen@live.ru>
2023-03-19 13:18:13 +0300

    05.Seminar

--- Collections.java
+++ Collections.java
@@ -1,7 +1,10 @@
-// ИЕРАРХИЯ КОЛЛЕКЦИИ (COLLECTION)
+//// ИЕРАРХИЯ КОЛЛЕКЦИИ (COLLECTION) //////////////
 import java.util.*;
+
+import javax.print.attribute.HashAttributeSet;
 public class Collections {
     public static void main(String[] args) {
+/////////////////// LIST //////////////////////////
 // Интерфейс List – (список) гибкая структура данных, позволяющая легко менять свой размер.
 // Пользователь этого интерфейса имеет точный контроль над тем, 
 // где в списке вставляется каждый элемент.Пользователь может обращаться к элементам по их целочисленному 
@@ -76,8 +79,8 @@ public class Collections {
         ll.add(5); // [4, 5]
         ll.get(1); // 5
         ll.getFirst(); // 4
-
-    // Интерфейс Queue - FIFO
+///////////// QUEUE ///////////////////////////////////
+// Интерфейс Queue - FIFO
         Queue<Integer> queue = new LinkedList<Integer>();
         queue.add(1); // [1]
         queue.add(2); // [1, 2]
@@ -97,7 +100,7 @@ public class Collections {
         deque.offerFirst(3); deque.offerLast(4); // [3, 4]
         deque.getFirst(); deque.getLast(); // 3 4 - возвращает, но не удаляет значения в деке, выдаст ошибку, если нет элементов
         deque.peekFirst(); deque.peekLast(); // 3 4 - возвращает, но не удаляет значения в деке, вернет null, если нет элементов
-    
+/////////// VECTOR ////////////////////////    
     // Класс Stack, расширение класса Vector. Stack представляет собой обработку данных по принципу LIFO.
     // Расширяет Vector пятью операциями, которые позволяют рассматривать вектор как стек.
         Stack<Integer> stack = new Stack<>();
@@ -107,6 +110,96 @@ public class Collections {
         stack.pop(); // 123
         stack.pop(); // 12
         stack.pop(); // 1
-    }
+////////////////// MAP /////////////////////
+// Интерфейс Map
+// Map – это множество коллекций, работающих с данными по принципу <Ключ / Значение>. У мапа нет индекса - в нем лежит hash, key, val, next.
+// В Map элементы располагаются как угодно и могут менять свое положение.
+// ● ускоренная обработка данных;
+// ● порядок добавления не запоминается
+// ● допускаются только уникальные ключи, значения могут повторяться;
+// ● помните про null значения*;
+// ● ускоренная обработка данных;
+// ● порядок добавления не запоминается.
+// Способы объявления Map:
+        Map<Integer,String> map1 = new HashMap<>(); // По умолчанию создается 16 ячеек, при заполнении 50% добавляется еще 16 ячеек 
+        Map<Integer,String> map2 = new HashMap<>(9); // Форсированно создаем мап с 9 ячейками и добавляем 9 при заполнении 50%
+        Map<Integer,String> map3 = new HashMap<>(9, 1.0f); // вторая переменная указывает 1.0, т.к. 100% заполнить прежде чем добавить новые ячейки.
+        Map<Integer, String> db = new HashMap<>();
+        db.put(1, "один"); // {1=один}
+        db.put(2, "два"); // {1=один, 2=два}
+        db.put(3, "три"); // 1=один, 2=два, 3=три}
+        db.put(31, "три один"); // {1=один, 2=два, 3=три, 31=три один}
+        db.put(13, "один три"); // {1=один, 2=два, 3=три, 13=один три, 31=три один}
+        db.put(null, "!null"); // {null=!null, 1=один, 2=два, 3=три, 13=один три, 31=три один}
+        db.put(null, null); // {null=null, 1=один, 2=два, 3=три, 13=один три, 31=три один}
+    // Класс HasTable
+    // «Устаревший брат» коллекции HashMap, который не знает про null
+        Map<Integer,String> table = new Hashtable<>();
+        table.put(1, "два");
+        table.put(11, "один один");
+        table.put(2, "один"); // {2=один, 1=два, 11=один один}
+        // table.put(null, "один"); // java.lang.NullPointerException
 
+    //  Класс HashMap 
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        hashMap.put(1, "one"); // {1=one}
+        hashMap.putIfAbsent(2, "two"); // {1=one, 2=two}
+        hashMap.put(3, "three"); // {1=one, 2=two, 3=three}
+        hashMap.put(4, "four"); // {1=one, 2=two, 3=three, 4=four}
+        hashMap.get(2); // two
+        hashMap.remove(2); // {1=one} - returns 2 and remove it key and value
+        hashMap.containsValue(2); // false
+        hashMap.containsKey(1); // true
+        hashMap.keySet(); // [1, 3, 4]
+        hashMap.values(); // [one, three, four] – возвращает набор значений
+        hashMap.entrySet(); // [1=one, 3=three, 4=four] - returns all keys and their values
+        hashMap.getOrDefault(21, "Key not found"); // Получить или написать "Key not found"
+        hashMap.replace(1, "2"); // аналог пута
+        hashMap.putAll(db);
+        // hashMap.forEach((k, v) -> System.out.println(k + v)); // принт all key+value
+        hashMap.compute(3, (k, va) -> va += "!"); // тут мы можем изменять значения хешмапа, проще форича
+        hashMap.compute(3, (k, va) -> String.valueOf(k)); // Замена ключа
+        hashMap.compute(3,  (k, va) -> va += "!" );
+        hashMap.computeIfPresent(3, (k, va) -> va += "!" ); // если существует ключ то делает
+        hashMap.computeIfAbsent(3, va -> "1" ); // делает только если нету
+        // hashMap.merge(5, "?", (k, vl) -> k+v);
+        Set<Integer> keys = hashMap.keySet(); // передать номера уникальных ключей  во множество
+        Iterator<Integer> iterator3 = hashMap.keySet().iterator(); // итератор это объект
+        while(iterator3.hasNext()){
+            String v = hashMap.get(iterator3.next()); // итератор собирает ссылки на элементы и в случае некст перебирает их
+           // System.out.printf("%s, ", v ); // null, один, два, 3!!, four, один три, три один, 
+            iterator3.remove(); // удаляет все
+        }
+    // Класс TreeMap
+    // В основе данной коллекции лежат красно-чёрные деревья. Позволяют быстрее искать, 
+    // но могут возникнуть «заминки» при добавлении.
+        TreeMap<Integer,String> tMap = new TreeMap<>();
+        tMap.put(1,"один"); // {1=один}
+        tMap.put(6,"шесть"); // {1=один, 6=шесть}
+        tMap.put(4,"четыре"); // {1=один, 4=четыре, 6=шесть}
+        tMap.put(3,"три"); // {1=один, 3=три, 4=четыре, 6=шесть}
+        tMap.put(2,"два"); // {1=один, 2=два, 3=три, 4=четыре, 6=шесть}
+        tMap.descendingKeySet(); // [6, 4, 3, 2, 1]
+        tMap.descendingMap(); // {6=шесть, 4=четыре, 3=три, 2=два, 1=один}
+        tMap.headMap(2); // {1=один}
+        tMap.tailMap(5); // {6=шесть}
+        tMap.firstEntry(); // 1=один
+        tMap.lastEntry(); // 6=шесть
+        tMap.headMap(2, true); // {1=один, 2=два} типа,срезов
+        tMap.tailMap(3, false); // {4=четыре, 6=шесть}
+        System.out.println(); //
+
+    // Класс LinkedHashMap
+    // «Старший брат» коллекции HashMap, который все помнит…
+    // Помнит порядок добавления элементов ➜ более медлительный
+        Map<Integer,String> linkmap = new LinkedHashMap<>();
+        linkmap.put(11, "один один"); //
+        linkmap.put(1, "два"); // 
+        linkmap.put(2, "один"); //  {11=один один, 1=два, 2=один}
+        Map<Integer,String> map = new HashMap<>();
+        map.put(11, "один один"); //
+        map.put(2, "два"); //
+        map.put(1, "один"); //  {1=один, 2=два, 11=один один}
+    
+    }
 }
